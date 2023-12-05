a935c9e1a579aa864edf5a8605bbb71b02ed4348
wolfSkullCave <67805222+wolfSkullCave@users.noreply.github.com>
2023-12-05 23:33:31 +0200

    Testing button events

--- calculator.js
+++ calculator.js
@@ -1,1 +1,21 @@
-console.log('hello')
+function testFunction () {
+    alert('hello world')
+}
+
+
+function addNumbers (num1, num2) {
+    return num1 + num2
+}
+
+
+function getUserNumbers () {
+    const usrNum1 = document.getElementById('usrNum1').value
+    
+    return usrNum1
+}
+
+
+console.log(addNumbers(1,1))
+
+const btnTest = document.querySelector('#test')
+btnTest.addEventListener('click', testFunction)
