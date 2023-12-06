b6fcc4a19e37d0f8b6e9fd76f648897180cd4d73
wolfSkullCave <67805222+wolfSkullCave@users.noreply.github.com>
2023-12-06 23:25:21 +0200

    Add button working.

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 function testFunction () {
-    alert('hello world')
+    return 'test function'
 }
 
 
@@ -9,13 +9,36 @@ function addNumbers (num1, num2) {
 
 
 function getUserNumbers () {
-    const usrNum1 = document.getElementById('usrNum1').value
-    
-    return usrNum1
+    const usrNum1 = Number(document.getElementById('usrNum1').value)
+    const usrNum2 = Number(document.getElementById('usrNum2').value)
+
+    const nums = [usrNum1, usrNum2]
+
+    return nums
 }
 
+function addNumbers () {
+    const nums = getUserNumbers()
+    const answer = nums.reduce( (total, nextItem) => total + nextItem, 0)
+
+    return answer
+}
 
 console.log(addNumbers(1,1))
+console.log(testFunction())
+
+const result = document.querySelector('#numAnswer')
 
 const btnTest = document.querySelector('#test')
-btnTest.addEventListener('click', testFunction)
+btnTest.addEventListener("click", () => {
+    const nums = getUserNumbers()
+    console.log(nums)
+})
+
+const addBtn = document.querySelector('#btnAdd')
+addBtn.addEventListener("click", () => {
+    const answer = addNumbers()
+
+    console.log(answer)
+    result.textContent = answer
+})
