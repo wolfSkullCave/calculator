d6411ce2a11440c025515a28085f64fa32596166
wolfSkullCave <67805222+wolfSkullCave@users.noreply.github.com>
2023-12-11 21:28:23 +0200

    Seven button works.

--- calculator.js
+++ calculator.js
@@ -1,21 +1,20 @@
-function testFunction () {
+function testFunction() {
     return 'test function'
 }
 
 
-function addNumbers (num1, num2) {
+function addNumbers(num1, num2) {
     return num1 + num2
 }
 
 
-function resetNumbers () {
-    document.getElementById('usrNum1').value = ''
-    document.getElementById('usrNum2').value = ''
+function resetNumbers() {
+   
 
 }
 
 
-function getUserNumbers () {
+function getUserNumbers() {
     const usrNum1 = Number(document.getElementById('usrNum1').value)
     const usrNum2 = Number(document.getElementById('usrNum2').value)
 
@@ -25,36 +24,59 @@ function getUserNumbers () {
 }
 
 
-function addNumbers () {
+function addNumbers() {
     const nums = getUserNumbers()
-    const answer = nums.reduce( (total, nextItem) => total + nextItem, 0)
+    const answer = nums.reduce((total, nextItem) => total + nextItem, 0)
 
     return answer
 }
 
 
-function subtractNumbers () {
+function subtractNumbers() {
     const nums = getUserNumbers()
-    const answer = nums.reduce( (total, nextItem) => total - nextItem)
+    const answer = nums.reduce((total, nextItem) => total - nextItem)
 
     return answer
 }
 
 
-function multiplyNumbers () {
+function multiplyNumbers() {
     const nums = getUserNumbers()
-    const answer = nums.reduce( (total, nextItem) => total * nextItem)
+    const answer = nums.reduce((total, nextItem) => total * nextItem)
 
     return answer
 }
 
 
-function divideNumbers () {
+function divideNumbers() {
     const nums = getUserNumbers()
-    const answer = nums.reduce( (total, nextItem) => total / nextItem)
+    const answer = nums.reduce((total, nextItem) => total / nextItem)
 
     return answer
 }
 
 
+function setDisplay(arr) {
+    if (arr.length > 0) result.textContent = arr
+
+}
+
+
+
+const myNums = [];
+
+const result = document.querySelector('#result')
+result.addEventListener('click', resetNumbers(myNums))
+
+const btnReset = document.querySelector('#btnAc')
+
+
+const btnSeven = document.querySelector('#btnSeven')
+btnSeven.addEventListener('click', () => {
+    myNums.push(7)
+    console.log(myNums)
+
+    setDisplay(myNums)
+
+})
 
