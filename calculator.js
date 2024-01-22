543f742417b11dd10e0e9d3ab90938cdcf1be668
wolfSkullCave <67805222+wolfSkullCave@users.noreply.github.com>
2024-01-22 22:02:07 +0200

    The rest of the buttons now work

--- calculator.js
+++ calculator.js
@@ -6,6 +6,15 @@ function operate() {
         case '+':
             answer = num1 + num2
             break
+        case '-':
+            answer = num1 - num2
+            break
+        case '*':
+            answer = num1 * num2
+            break
+        case '/':
+            answer = num1 / num2
+            break
         default:
             console.log(num1, operator, num2)
     }
@@ -50,7 +59,7 @@ let operator = null
 let num2 = null
 
 const numArr = []
-
+// resultL = document.querySelector('#resultL').textContent
 
 btnAc = document.querySelector('#btnAc')
 btnAc.addEventListener('click', resetAll)
@@ -74,6 +83,55 @@ btnThree.addEventListener('click', () => {
     setDisplay(numArr.join(''))
 })
 
+btnFour = document.querySelector('#btnFour')
+btnFour.addEventListener('click', () => {
+    numArr.push(4)
+    setDisplay(numArr.join(''))
+})
+
+btnFive = document.querySelector('#btnFive')
+btnFive.addEventListener('click', () => {
+    numArr.push(5)
+    setDisplay(numArr.join(''))
+})
+
+btnSix = document.querySelector('#btnSix')
+btnSix.addEventListener('click', () => {
+    numArr.push(6)
+    setDisplay(numArr.join(''))
+})
+
+btnSeven = document.querySelector('#btnSeven')
+btnSeven.addEventListener('click', () => {
+    numArr.push(7)
+    setDisplay(numArr.join(''))
+})
+
+btnEight = document.querySelector('#btnEight')
+btnEight.addEventListener('click', () => {
+    numArr.push(8)
+    setDisplay(numArr.join(''))
+})
+
+btnNine = document.querySelector('#btnNine')
+btnNine.addEventListener('click', () => {
+    numArr.push(9)
+    setDisplay(numArr.join(''))
+})
+
+btnZero = document.querySelector('#btnZero')
+btnZero.addEventListener('click', () => {
+    numArr.push(0)
+    setDisplay(numArr.join(''))
+})
+
+btnPoint = document.querySelector('#btnPoint')
+btnPoint.addEventListener('click', () => {
+    numArr.push('.')
+    setDisplay(numArr.join(''))
+})
+
+
 // operators
 btnAdd = document.querySelector('#btnAdd')
 btnAdd.addEventListener('click', () => {
@@ -81,7 +139,34 @@ btnAdd.addEventListener('click', () => {
     setDisplay(operator)
     num1 = Number(numArr.join(''))
     numArr.length = 0
+
 })
+
+btnSubtract = document.querySelector('#btnSubtract')
+btnSubtract.addEventListener('click', () => {
+    operator = '-'
+    setDisplay(operator)
+    setDisplay(operator)
+    num1 = Number(numArr.join(''))
+    numArr.length = 0
+})
+
+btnMultiply = document.querySelector('#btnMultiply')
+btnMultiply.addEventListener('click', () => {
+    operator = '*'
+    setDisplay(operator)
+    num1 = Number(numArr.join(''))
+    numArr.length = 0
+})
+
+btnDivide = document.querySelector('#btnDivide')
+btnDivide.addEventListener('click', () => {
+    operator = '/'
+    setDisplay(operator)
+    num1 = Number(numArr.join(''))
+    numArr.length = 0
+})
+
 
 // answer
 btnEquals = document.querySelector('#btnEquals')
